39f22bfb830a9deb2b9c80d946798e585bc5a6f7
TrollKiller1234 <pipivelky@gmail.com>
2024-09-23 11:52:28 +0200

    Pridanie novych podstranok: odhlasenie,prihlasenie,registracia,gdpr,hladanie,notifikacie,o-nas,pridat,prispevok,prispevok/[id],profil,profil/[id]
    do kazdeho urobene page.tsx so zakladnou strukturov stranky

---
+++ src/app/auth/prihlasenie/page.tsx
@@ -0,0 +1,17 @@
+// Prihlasenie
+
+// src/app/auth/prihlasenie/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: 'Prihlasenie | ZoskaSnap'}
+
+export default function LogIn() {
+
+  return(
+    <Container>
+      <Typography> Prihlasenie </Typography>
+    </Container>
+  );
+}
---
+++ src/app/auth/registracia/page.tsx
@@ -0,0 +1,17 @@
+// Registracia
+
+// src/app/auth/registracia/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: 'Registracia | ZoskaSnap'}
+
+export default function SignUp() {
+
+  return(
+    <Container>
+      <Typography> Registracia </Typography>
+    </Container>
+  );
+}
