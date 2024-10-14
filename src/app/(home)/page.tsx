d5d8a204a537b079ba1fb8cea2c486ed9b1277c6
TrollKiller1234 <pipivelky@gmail.com>
2024-10-14 13:17:47 +0200

    nejde autentifikacia

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,24 @@
+'use client';
 
-// src/app/page.tsx
+import { useSession } from 'next-auth/react';
+import NonAuthHomeView from '@/sections/NonAuthHomeView';
+import AuthHomeView from '@/sections/AuthHomeView';
 
-import Typography from "@mui/material/Typography";
+const HomePage: React.FC = () => {
+  const { data: session, status } = useSession();
 
-export const metadata = { title: 'Domov | ZoskaSnap'}
+  if (status === 'loading') {
+    // You can return a loading spinner while session is being fetched
+    return <div>Loading...</div>;
+  }
 
-export default function Home() {
+  if (session) {
+    // User is authenticated, show the Authenticated Home View
+    return <AuthHomeView />;
+  } else {
+    // User is not authenticated, show the Non-Authenticated Home View
+    return <NonAuthHomeView />;
+  }
+};
 
-  return(
-
-      <Typography> Domovska stranka </Typography>
-
-  );
-}
-
+export default HomePage;
